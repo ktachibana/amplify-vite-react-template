a25529e536a59c7f37c7dfa98cfb53514487dcdc
Kenichi Tachibana <deadzone@be.to>
2025-02-09 17:35:57 +0900

    TODO削除と認証を追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
+import {Amplify} from "aws-amplify";
+import {Authenticator} from "@aws-amplify/ui-react";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Authenticator>
+            <App/>
+        </Authenticator>
+    </React.StrictMode>
 );
